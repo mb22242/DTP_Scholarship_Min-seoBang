62c457d2ba1cedea37909321ab537600a0a6e734
mb22242 <mb22242@my.westlake.school.nz>
2025-07-28 13:57:38 +1200

    Created search function that works - with the help of Bevis

--- Final/app.py
+++ Final/app.py
@@ -87,9 +87,35 @@ def report():
 @login_required
 def admin():
     conn = get_db_connection()
-    items = conn.execute('SELECT id, item_name, name, description, image_filename, category FROM items').fetchall()
+    item = request.args.get('item')
+    
+    if item != 'Name:':
+        sql = 'SELECT * FROM items WHERE item_name = ?'
+        items = conn.execute(sql, (item,)).fetchall()
+    
+    else:
+        # sql = 'SELECT * FROM items WHERE item_name = ?'
+        # items = conn.execute(sql).fetchall()
+        items = conn.execute('SELECT id, item_name, name, description, image_filename, category FROM items').fetchall()
+    
     conn.close()
     return render_template('admin.html', username=current_user.username, items=items)
+
+# @app.route('/search')
+# def search():
+#     item = request.args.get('item')
+#     conn = get_db_connection
+    
+#     if item != 'Name:':
+#         sql = 'SELECT * FROM items WHERE item_name = ?'
+#         items = conn.execute(sql, (item,)).fetchall()
+#     # else:
+#     #     sql = 'SELECT * FROM items WHERE item_name = ?'
+#     #     items = conn.execute(sql).fetchall()
+    
+#     conn.close()
+    
+#     return render_template('admin.html', items=items)
 
 
 @app.route('/admin/upload', methods=['GET', 'POST'])
